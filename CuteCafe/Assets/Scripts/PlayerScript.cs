c898b1afb629a67cef627c34083de5dcf4fa2662
Fabiano <fabianocoelho06@gmail.com>
2019-05-18 18:42:28 -0300

    [ADD] Menu e Assets de Comida
    Criado canvas do menu principal com background, painéis de como jogar e de créditos, botões para acessá-los e sair deles, botões para ir para a Scene principal do jogo e botão para sair.

---
+++ CuteCafe/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject youWin;
+    public GameObject youLose;
+
+    private bool gameStarted = false;
+    private bool movement = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = true;
+        gameStarted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /*private void OnCollisionEnter2D(Collision2D collision){
+        if(!gameEnded){
+            if(collision.collider.tag == "Espinhos"){
+                youLose.SetActive(true);
+                gameEnded = true;
+                movement = false;
+            }
+
+            if(collision.collider.tag == "Landing"){
+                youWin.SetActive(true);
+                gameEnded = true;
+                movement = false;
+            }
+        }
+    }*/
+
+    public void PlayAgain(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
